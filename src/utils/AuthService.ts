a09c0ba1f66055e05a6fd4c5aa3b90849ab5fe9a
Miziev <mizzzev@outlook.com>
2024-02-27 13:20:14 +0200

    returned all auth functions to auth service file & deleted unnecessary files

--- src/utils/AuthService.ts
+++ src/utils/AuthService.ts
@@ -1,4 +1,37 @@
-import { registerUser } from './services/registerUser';
-import { loginUser } from './services/loginUser';
+import axios from "axios";
 
-export { registerUser, loginUser };
+const BASE_URL = "http://13.60.5.92:8080/api/auth";
+
+interface RegistrationData {
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+export async function registerUser(userData: RegistrationData) {
+    try {
+        const response = await axios.post(`${BASE_URL}/register`, userData);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+interface LoginData {
+    usernameOrEmail: string;
+    password: string;
+}
+
+export async function loginUser(userData: LoginData) {
+    try {
+        const response = await axios.post(`${BASE_URL}/login`, userData);
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
--- src/utils/services/apiConfig.ts
+++
@@ -1,4 +0,0 @@
-// apiConfig.ts
-const BASE_URL = "http://13.60.5.92:8080/api/auth";
-
-export {BASE_URL};
