3e348d08b3ad65a6ce9ac31a84bbfc3d34c737ab
Alex Stefanskyi <stefanskioleksi@gmail.com>
2024-02-28 01:07:23 +0100

    created saveToken getToken removeToken logout isUserLoggedIn functions

--- src/utils/AuthService.ts
+++ src/utils/AuthService.ts
@@ -3,35 +3,55 @@ import axios from "axios";
 const BASE_URL = "http://13.60.5.92:8080/api/auth";
 
 interface RegistrationData {
-    firstName: string;
-    lastName: string;
-    username: string;
-    email: string;
-    password: string;
+	firstName: string;
+	lastName: string;
+	username: string;
+	email: string;
+	password: string;
 }
 
 export async function registerUser(userData: RegistrationData) {
-    try {
-        const response = await axios.post(`${BASE_URL}/register`, userData);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+	try {
+		const response = await axios.post(`${BASE_URL}/register`, userData);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 }
 
 interface LoginData {
-    usernameOrEmail: string;
-    password: string;
+	usernameOrEmail: string;
+	password: string;
 }
 
 export async function loginUser(userData: LoginData) {
-    try {
-        const response = await axios.post(`${BASE_URL}/login`, userData);
-        console.log(response.data)
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+	try {
+		const response = await axios.post(`${BASE_URL}/login`, userData);
+		console.log(response.data)
+		return response.data;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+
+export function saveToken(token: string) {
+	localStorage.setItem('token', token);
+}
+
+export function getToken() {
+	return localStorage.getItem('token');
+}
+
+export function removeToken() {
+	localStorage.removeItem('token');
+}
+
+export function logout() {
+	removeToken();
+}
+
+export function isUserLoggedIn() {
+	return !!getToken();
 }
