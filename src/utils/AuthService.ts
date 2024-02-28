a6085e3eb272e11e40d88c2611b7e5e21a0698eb
Miziev <mizzzev@outlook.com>
2024-02-28 15:46:19 +0200

    created all functions for auth services (saveLoggedinUser(), isUserLoggedIn(), getLoggedInUser()) improve function logout()

--- src/utils/AuthService.ts
+++ src/utils/AuthService.ts
@@ -3,55 +3,87 @@ import axios from "axios";
 const BASE_URL = "http://13.60.5.92:8080/api/auth";
 
 interface RegistrationData {
-	firstName: string;
-	lastName: string;
-	username: string;
-	email: string;
-	password: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
 }
 
 export async function registerUser(userData: RegistrationData) {
-	try {
-		const response = await axios.post(`${BASE_URL}/register`, userData);
-		return response.data;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
+    try {
+        const response = await axios.post(`${BASE_URL}/register`, userData);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
 
+
 interface LoginData {
-	usernameOrEmail: string;
-	password: string;
+    usernameOrEmail: string;
+    password: string;
 }
 
 export async function loginUser(userData: LoginData) {
-	try {
-		const response = await axios.post(`${BASE_URL}/login`, userData);
-		console.log(response.data)
-		return response.data;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
+    try {
+        const response = await axios.post(`${BASE_URL}/login`, userData);
+
+        saveToken(response.data.accessToken);
+
+        console.log(response.data)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+
+export const saveLoggedInUser = (username: string, role: string) => {
+    console.log("saved username - "+username+"saved role - "+role);
+
+    sessionStorage.setItem("authenticatedUser", username);
+    sessionStorage.setItem("role", role);
+}
+
+export const isUserLoggedIn = () => {
+    const username = sessionStorage.getItem("authenticatedUser");
+
+    if (username == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export const getLoggedInUser = () => {
+    const username = sessionStorage.getItem("authenticatedUser");
+    console.log("username saved - " + username);
+    return username as string;
 }
 
 export function saveToken(token: string) {
-	localStorage.setItem('token', token);
+    localStorage.setItem('token', token);
 }
 
-export function getToken() {
-	return localStorage.getItem('token');
-}
-
-export function removeToken() {
-	localStorage.removeItem('token');
+export function getToken(): string | null {
+    console.log("get token - " + localStorage.getItem('token'))
+    return localStorage.getItem('token');
 }
 
 export function logout() {
-	removeToken();
+    localStorage.clear();
+    sessionStorage.clear();
 }
 
-export function isUserLoggedIn() {
-	return !!getToken();
+export const isAdminUser = () => {
+    let role = sessionStorage.getItem("role");
+
+    if (role != null && role === "ROLE_ADMIN") {
+        return true;
+    } else {
+        return false;
+    }
 }
