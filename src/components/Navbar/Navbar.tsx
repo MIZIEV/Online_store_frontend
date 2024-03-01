b4ff0c94102192e0497bb3c70e1b38d9e305ba75
Alex Stefanskyi <stefanskioleksi@gmail.com>
2024-03-01 13:17:31 +0100

    added log out button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
-//Navbar.tsx
-
-import { NavLink } from "react-router-dom";
-import { getLoggedInUser } from "../../utils/AuthService";
+// Navbar.tsx
+import { NavLink, useNavigate } from "react-router-dom";
+import { getLoggedInUser, logout } from "../../utils/AuthService"; // Додана функція logout
 import Button from "../../UI/Button/Button";
 import classes from "./Navbar.module.scss";
 
 const Navbar = () => {
 	const loggedInUser = getLoggedInUser();
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		logout(); // Викликаємо функцію logout при кліку на кнопку LogOut
+		navigate('/'); // Перенаправляємо користувача на сторінку входу після виходу
+	};
 
 	return (
 		<nav className={classes.navbar}>
@@ -15,15 +20,21 @@ const Navbar = () => {
 			</NavLink>
 			<div className={classes["buttons-wrapper"]}>
 				{loggedInUser && <div className={classes["welcome-message"]}>Welcome, {loggedInUser}</div>}
-				<NavLink to="/admin">
-					<Button>ADMIN</Button>
-				</NavLink>
-				<NavLink to="/signin">
-					<Button>Sign in</Button>
-				</NavLink>
-				<NavLink to="/signup">
-					<Button>Sign up</Button>
-				</NavLink>
+				{loggedInUser ? ( // Перевіряємо, чи користувач увійшов у систему
+					<Button onClick={handleLogout}>Log Out</Button> // Якщо так, то відображаємо кнопку LogOut
+				) : (
+					<> {/* Якщо користувач не увійшов у систему, відображаємо кнопки для входу та реєстрації */}
+							<NavLink to="/admin">
+								<Button>ADMIN</Button>
+							</NavLink>
+						<NavLink to="/signin">
+							<Button>Sign in</Button>
+						</NavLink>
+						<NavLink to="/signup">
+							<Button>Sign up</Button>
+						</NavLink>
+					</>
+				)}
 			</div>
 		</nav>
 	);
