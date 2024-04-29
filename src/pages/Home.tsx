3db895da0e1da54bb30cea75d4b010a170327b96
Miziev <mizzzev@outlook.com>
2024-04-29 17:56:51 +0300

    - added subscribe block with tweaks in style file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -126,6 +126,32 @@ const HomePage = () => {
       {isPending && <p>Loading...</p>}
       {isError && "Failed to fetch data"}
 
+      <div className={classes.subscribeBlock}>
+
+        <div className={classes.iconBlock}>
+          <svg width="80" height="80" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M90.0023 45.0023L0 0L12.15 40.5023H60.7523V49.5023H12.15L0.00224984 90.0023L90.0023 45.0023Z" fill="#0D0C0C" />
+          </svg>
+        </div>
+
+        <div className={classes.textBlock}>
+          <h4 className={classes.topText}>Підпишись на розсилку</h4>
+          <h5 className={classes.bottomText}>та першим дізнайся про нові Б/У надходження</h5>
+        </div>
+
+        <div className={classes.inputBlock}>
+          <input type="text" placeholder="Введіть пошту" />
+          <button >
+            Підписатись
+            <svg width="20" height="21" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 12.5H19" stroke="#F7F8FA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M12 5.5L19 12.5L12 19.5" stroke="#F7F8FA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </button>
+        </div>
+
+      </div>
+
     </div>
   );
 };
