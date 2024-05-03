f7649c2e83b3647a37fa2e9d3295fd5b138b78ae
Miziev <mizzzev@outlook.com>
2024-05-03 13:31:59 +0300

    - changed color inside input fields to black - added all fields to Phone type - continue work with addNewPhoneComponent (now it works)

--- src/components/AdminMenu/AdminModals/AddNewPhoneComponent.tsx
+++ src/components/AdminMenu/AdminModals/AddNewPhoneComponent.tsx
@@ -2,7 +2,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { postProduct, queryClient } from "../../../utils/http";
 import classes from "./AddNewPhoneComponent.module.scss"
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, Switch } from "@mui/material";
 import { useState } from "react";
 
 const AddNewPhoneComponent = () => {
@@ -16,20 +16,30 @@ const AddNewPhoneComponent = () => {
     },
   });
 
+  const [brand, setBrand] = useState<string>("");
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.target as HTMLFormElement);
-    const data = Object.fromEntries(formData);
+
+
+    const data = {
+      brand: brand,
+      ...Object.fromEntries(formData),
+    };
+
 
     mutate(data);
   };
 
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  //const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
 
-  const handleOptionChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-      setSelectedOptions(event.target.value as string[]);
-  };
+  /* const handleOptionChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+     setSelectedOptions(event.target.value as string[]);
+   };*/
+
+  const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 
 
@@ -46,6 +56,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="model"
+              name="model"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -53,6 +64,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="main picture URL"
+              name="mainPictureURL"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -60,6 +72,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="operation system"
+              name="os"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -67,6 +80,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="version of operation system "
+              name="osVersion"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -74,6 +88,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="screen size"
+              name="screenSize"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -81,6 +96,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="resolution"
+              name="resolution"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -88,6 +104,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="main camera"
+              name="mainCamera"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -95,6 +112,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="front camera"
+              name="frontCamera"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -102,6 +120,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="processor"
+              name="processor"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -109,6 +128,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="count of cores"
+              name="countOfCores"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -116,6 +136,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="ram"
+              name="ram"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -123,6 +144,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="rom"
+              name="rom"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -130,6 +152,7 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="weight"
+              name="weight"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -137,13 +160,15 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="battery capacity"
+              name="batteryCapacity"
             />
           </div>
           <div className={classes.inputContainer}>
             <input
               className={classes.inputField}
               type="text"
-              placeholder="count O fSim Card"
+              placeholder="count Of Sim Card"
+              name="countOfSimCard"
             />
           </div>
           <div className={classes.inputContainer}>
@@ -151,15 +176,37 @@ const AddNewPhoneComponent = () => {
               className={classes.inputField}
               type="text"
               placeholder="price"
+              name="price"
             />
           </div>
-          
-          <Select label="Num of sim">
-            <MenuItem value={1}>1</MenuItem>
-            <MenuItem value={2}>2</MenuItem>
-          </Select>
 
-          <FormControl fullWidth>
+          <div className={classes.inputContainer}>
+
+            <Select label="Num of sim">
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+            </Select>
+          </div>
+
+          <div className={classes.inputContainer}>
+
+            <Select label="Brand"
+              value={brand}
+              onChange={(e) => setBrand(e.target.value as string)}>
+              <MenuItem value="SAMSUNG">Samsung</MenuItem>
+              <MenuItem value="XIAOMI">Xiaomi</MenuItem>
+              <MenuItem value="APPLE">Apple</MenuItem>
+            </Select>
+
+          </div>
+
+          <div className={classes.inputContainer}>
+            <FormGroup>
+              <FormControlLabel control={<Switch />} label="Is used?" />
+            </FormGroup>
+          </div>
+
+          {/*<FormControl fullWidth>
             <InputLabel id="multi-select-dropdown-label">Select Options</InputLabel>
             <Select
                 labelId="multi-select-dropdown-label"
@@ -173,8 +220,8 @@ const AddNewPhoneComponent = () => {
                 <MenuItem value="Option 2">Option 2</MenuItem>
                 <MenuItem value="Option 3">Option 3</MenuItem>
             </Select>
-        </FormControl>
-        
+        </FormControl>*/}
+
 
         </div>
 
