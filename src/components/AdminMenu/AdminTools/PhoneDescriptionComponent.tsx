30c8893f0fccc55c292b4095406caa48ecbf3ef0
Miziev <mizzzev@outlook.com>
2024-05-06 19:00:19 +0300

    - added delete button in description item with some style tweaks

--- src/components/AdminMenu/AdminTools/PhoneDescriptionComponent.tsx
+++ src/components/AdminMenu/AdminTools/PhoneDescriptionComponent.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import classes from "./PhoneDescriptionComponent.module.scss";
-import { addNewDescription, getDescriptions } from "../../../utils/descriptionService";
+import { addNewDescription, deleteDescription, getDescriptions } from "../../../utils/descriptionService";
 import { useParams } from "react-router";
+
 
 const PhoneDescriptionComponent: React.FC = () => {
 
@@ -22,7 +23,20 @@ const PhoneDescriptionComponent: React.FC = () => {
         const formData = new FormData(e.target as HTMLFormElement);
 
         try {
-            addNewDescription(phoneId, formData);
+            await addNewDescription(phoneId, formData);
+            const updatedDescriptionList = await getDescriptions(phoneId);
+            setDescriptionList(updatedDescriptionList)
+            setDescriptionText("");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    const deleteDescriptionText = async (descriptionId: number) => {
+
+        try {
+            await deleteDescription(phoneId, descriptionId);
             const updatedDescriptionList = await getDescriptions(phoneId);
             setDescriptionList(updatedDescriptionList)
             setDescriptionText("");
@@ -47,7 +61,18 @@ const PhoneDescriptionComponent: React.FC = () => {
                 {descriptionList ? (
                     <ol>
                         {descriptionList.map((description) => (
-                            <li className={classes.descriptionItem} key={description.id}>{description.descriptionText}</li>
+
+                            <li className={classes.descriptionItem} key={description.id}>
+                                <div className={classes.descriptionText}>
+                                    {description.descriptionText}
+                                </div>
+
+                                <svg onClick={() => deleteDescriptionText(description.id)} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M42 8H32C32 5.794 30.206 4 28 4H20C17.794 4 16 5.794 16 8H6V12H42V8Z" />
+                                    <path d="M22 36H18V16H10V40.286C10 42.334 11.794 44 14 44H34C36.206 44 38 42.334 38 40.286V16H30V36H26V16H22V36Z" />
+                                </svg>
+                            </li>
+
                         ))}
                     </ol>
                 ) : (<div>error</div>)}
