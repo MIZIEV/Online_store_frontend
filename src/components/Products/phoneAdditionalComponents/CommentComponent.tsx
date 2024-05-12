cb5fbdb3a04b72b2096ed17a6b81a7afab2040da
Miziev <mizzzev@outlook.com>
2024-05-12 22:44:25 +0300

    - added functional for deleting and adding comments

--- src/components/Products/phoneAdditionalComponents/CommentComponent.tsx
+++ src/components/Products/phoneAdditionalComponents/CommentComponent.tsx
@@ -3,17 +3,43 @@ import classes from "./CommentComponent.module.scss"
 
 const CommentComponent: React.FC = (props) => {
 
-    const comment = props.comment;
+    const { id, comment, updateComments, onDelete } = props;
 
+    const handleDeleteComment = async () => {
 
+        try {
+            await onDelete(comment.id);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const formatCreatedAt = (createdAt: string): string => {
+        const date = new Date(createdAt);
+        const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`; // Format the date as YYYY-MM-DD
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${formattedDate} ${hours}:${minutes}`;
+    }
 
     return (
         <div className={classes.container}>
             <div className={classes.topBlock}>
                 <span className={classes.author}>{comment.authorName}</span>
-                <span className={classes.createdAt}>{comment.createdAt}</span>
+                <span className={classes.createdAt}>{formatCreatedAt(comment.createdAt)}</span>
             </div>
-            <div className={classes.bottomBlock}>{comment.commentText}</div>
+            <div className={classes.bottomBlock}>
+                <div className={classes.comment}>
+                    {comment.commentText}
+
+                </div>
+                <div className={classes.deleteIcon}>
+                    <svg onClick={handleDeleteComment} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M42 8H32C32 5.794 30.206 4 28 4H20C17.794 4 16 5.794 16 8H6V12H42V8Z" />
+                        <path d="M22 36H18V16H10V40.286C10 42.334 11.794 44 14 44H34C36.206 44 38 42.334 38 40.286V16H30V36H26V16H22V36Z" />
+                    </svg>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Products/phoneAdditionalComponents/ReviewsComponent.tsx
+++ src/components/Products/phoneAdditionalComponents/ReviewsComponent.tsx
@@ -2,21 +2,49 @@ import React, { useEffect, useState } from "react";
 import classes from "./RewievsComponent.module.scss"
 import CommentComponent from "./CommentComponent";
 import { Comment } from "../../../shared.types";
-import { getAllComments } from "../../../utils/CommentsServvice";
+import { addNewComment, deleteComment, getAllComments } from "../../../utils/CommentsServvice";
 import { useParams } from "react-router";
 
 const ReviewsComponent: React.FC = () => {
 
     const [comments, setComments] = useState<Comment[]>([]);
+    const [commentText, setCommentText] = useState<string>("");
+
     const { id } = useParams();
 
     useEffect(() => {
         getAllComments(id).then((response) => {
             setComments(response);
         })
-
     }, [])
 
+
+    const handleNewComment = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const commentText = formData.get("commentText") as string;
+        const comment: Comment = { commentText };
+
+        try {
+            await addNewComment(comment, id);
+            const updtedComments = await getAllComments(id);
+
+            setComments(updtedComments);
+            setCommentText("");
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleDeleteComment = async (commentId: number) => {
+        try {
+            await deleteComment(id, commentId); // Call the deleteComment function
+            setComments(comments.filter(comment => comment.id !== commentId)); // Filter out the deleted comment
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
 
 
@@ -24,11 +52,16 @@ const ReviewsComponent: React.FC = () => {
         <div className={classes.container}>
 
             <div className={classes.topBlock}>
-                <input
-                    type="text"
-                    placeholder="Запишіть свій відгук про цей товар"
-                />
-                <button>Написати відгук</button>
+                <form onSubmit={handleNewComment}>
+                    <input
+                        type="text"
+                        value={commentText}
+                        name="commentText"
+                        onChange={(e) => setCommentText(e.target.value as string)}
+                        placeholder="Запишіть свій відгук про цей товар"
+                    />
+                    <button type="submit">Написати відгук</button>
+                </form>
             </div>
 
             <div className={classes.bottomBlock}>
@@ -36,7 +69,12 @@ const ReviewsComponent: React.FC = () => {
                 {
                     comments.map((comment) => (
 
-                        <CommentComponent key={comment.id} comment={comment} />
+                        <CommentComponent
+                            id={id}
+                            updateComments={() => getAllComments(id)}
+                            onDelete={handleDeleteComment}
+                            key={comment.id}
+                            comment={comment} />
                     ))
                 }
 
