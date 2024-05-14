62b8937e5d0a829fda9119471024f02519ac1e39
Miziev <mizzzev@outlook.com>
2024-05-14 22:16:54 +0300

    - added breadCrumbs to catalog page

--- src/pages/PhoneCatalog.tsx
+++ src/pages/PhoneCatalog.tsx
@@ -5,6 +5,7 @@ import CatalogCard from "../components/Card/CatalogCard";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "../utils/http";
 import { NavLink } from "react-router-dom";
+import BreadCrumb from "../components/BreadCrumb/BreadCrumb";
 
 const PhoneCatalog: React.FC = () => {
 
@@ -17,33 +18,39 @@ const PhoneCatalog: React.FC = () => {
 
     return (
         <div className={classes.container}>
-            <div className={classes.leftBlock}>
-
-                <CheckBoxBlock title="Бренд" />
-                <CheckBoxBlock title="Модель смартфона" />
-                <CheckBoxBlock title="Стан" />
-                <CheckBoxBlock title="Діагональ екрану" />
-                <CheckBoxBlock title="Роздільна здатність екрану" />
-                <CheckBoxBlock title="Оперативна пам'ять" />
-                <CheckBoxBlock title="Обсяг пам'яті" />
-                <CheckBoxBlock title="Кількість ядер" />
-                <CheckBoxBlock title="Кількість SIM-карт" />
+            <div className={classes.breadCrumb}>
+                <BreadCrumb items={[{ path: "/", title: "Головна" }, { path: "/phone/catalog", title: "/товари" }]} />
             </div>
 
-            <div className={classes.rightBlock}>
-                {data && data.length > 0 ? (
-                    data.map((phone) => (
-                        <NavLink to={`/phone/${phone.id}`} className={classes.link}>
+            <h1>Товари</h1>
 
-                            <CatalogCard phoneData={phone} />
-                        </NavLink>
-                    ))
-                ) : (
-                    <div>No data</div>
-                )}
+            <div className={classes.pageContent}>
 
+                <div className={classes.leftBlock}>
+
+                    <CheckBoxBlock title="Бренд" />
+                    <CheckBoxBlock title="Модель смартфона" />
+                    <CheckBoxBlock title="Стан" />
+                    <CheckBoxBlock title="Діагональ екрану" />
+                    <CheckBoxBlock title="Роздільна здатність екрану" />
+                    <CheckBoxBlock title="Оперативна пам'ять" />
+                    <CheckBoxBlock title="Обсяг пам'яті" />
+                    <CheckBoxBlock title="Кількість ядер" />
+                    <CheckBoxBlock title="Кількість SIM-карт" />
+                </div>
+
+                <div className={classes.rightBlock}>
+                    {data && data.length > 0 ? (
+                        data.map((phone) => (
+                            <NavLink to={`/phone/${phone.id}`} className={classes.link}>
+                                <CatalogCard phoneData={phone} />
+                            </NavLink>
+                        ))
+                    ) : (
+                        <div>No data</div>
+                    )}
+                </div>
             </div>
-
         </div>
     )
 }
