354659916853b2790153f82ab4f3dbc763fc1ffc
Miziev <mizzzev@outlook.com>
2024-05-24 19:00:40 +0300

    - change logic inside Phone Card in Cart button (now when you add Phone to cart cart icon replaced to check mark and if you delete Phone from cart, previous icon come back)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,25 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../UI/Button/Button";
 import { Phone } from "../../shared.types";
 import classes from "./Card.module.scss";
-import { CartProduct, addToCart } from "../../redux/cartSlice";
+import { CartProduct, addToCart, removeFromCart } from "../../redux/cartSlice";
 import { useNavigate } from "react-router";
+import { RootState } from "@reduxjs/toolkit/query";
 
 const Card: React.FC<Phone> = (props) => {
   const dispatch = useDispatch();
   const navigator = useNavigate();
 
+  const isAdded = useSelector((state: RootState) =>
+    state.cart.items.some((item) => item.id === props.id)
+  );
+
   const addProduct = (payload: CartProduct) => {
     dispatch(addToCart(payload));
+  };
+
+  const removeProduct = (id: number) => {
+    dispatch(removeFromCart({ id }));
   };
 
   const handleNavigateOnClick = () => {
@@ -30,21 +39,46 @@ const Card: React.FC<Phone> = (props) => {
           <span className={classes.price}>₴ {props.price}</span>
           <Button
             onClick={() =>
-              addProduct({
-                id: props.id,
-                brand: props.brand,
-                model: props.model,
-                price: props.price,
-                quantity: 1,
-                image: props.mainPictureURL,
-              })
+              isAdded
+                ? removeProduct(props.id)
+                : addProduct({
+                    id: props.id,
+                    brand: props.brand,
+                    model: props.model,
+                    price: props.price,
+                    quantity: 1,
+                    image: props.mainPictureURL,
+                  })
             }
           >
-            <svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.3245 2.135L5.4285 11.448L2.9615 17H8H14H18V15H6.039L7.15 12.5H17.2205L20.3875 3H4.7205L3.7205 0H0V2H2.1665L2.3245 2.135Z" fill="#12372A" />
-            </svg>
+            {isAdded ? (
+              <svg
+                width="21"
+                height="17"
+                viewBox="0 0 21 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.5 12.5L4 9L2.5 10.5L7.5 15.5L18.5 4.5L17 3L7.5 12.5Z"
+                  fill="#12372A"
+                />
+              </svg>
+            ) : (
+              <svg
+                width="21"
+                height="17"
+                viewBox="0 0 21 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2.3245 2.135L5.4285 11.448L2.9615 17H8H14H18V15H6.039L7.15 12.5H17.2205L20.3875 3H4.7205L3.7205 0H0V2H2.1665L2.3245 2.135Z"
+                  fill="#12372A"
+                />
+              </svg>
+            )}
           </Button>
-
         </div>
       </div>
     </div>
