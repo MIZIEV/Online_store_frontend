6d0f398f97eb6a9468669eec6af99fef78f514e7
Miziev <mizzzev@outlook.com>
2024-05-27 19:49:46 +0300

    - created wishlistItem with style file & implemented it on wishList component

--- src/components/UserPageAdditionalComponents/WishList.tsx
+++ src/components/UserPageAdditionalComponents/WishList.tsx
@@ -1,9 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { Phone } from "../../shared.types";
+import { getWishListForUser } from "../../utils/UserService";
+import WishListItem from "./WishListItem";
+import classes from "./WishList.module.scss"
 
 const WishList: React.FC = () => {
+
+    const [wishList, setWishList] = useState<Phone[]>([])
+    const username = sessionStorage.getItem("authenticatedUserName");
+
+    useEffect(() => {
+
+        getWishListForUser(username).then((response) => {
+            setWishList(response);
+        })
+    }, [])
+
+    const deletePhoneHandler = (phoneId: number) => {
+        setWishList((prevWishList) => prevWishList.filter((phone) => phone.id !== phoneId));
+    };
+
     return (
-        <div>
-            WishListComponent
+        <div className={classes.container}>
+            {
+                wishList.map((phone) => (
+                    <WishListItem key={phone.id} phone={phone} onDelete={deletePhoneHandler} />
+                ))
+            }
         </div>
     )
 }
