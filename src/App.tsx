4a9640e8c97313d890b68dde6d66d4058a0dd4b7
Miziev <mizzzev@outlook.com>
2024-06-11 14:36:59 +0300

    - created order managment component & implemented that functional in app - added function in order service for changing complete status

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import PhoneCatalog from "./pages/PhoneCatalog";
 import UserPersonalPage from "./pages/UserPersonalPage";
 import BlogSettingComponent from "./components/AdminMenu/BlogDashboard/BlogSettingComponent";
 import FeaturesManagment from "./components/AdminMenu/AdminTools/FeaturesManagment";
+import OrdersManagment from "./components/AdminMenu/AdminTools/OrdersManagment";
 
 const router = createBrowserRouter([
   {
@@ -126,6 +127,11 @@ const router = createBrowserRouter([
       {
         path: "admin/phone-managment/:phoneId/features",
         element: <FeaturesManagment />,
+        children: [{ path: "cart", element: <CartModal /> }]
+      },
+      {
+        path: "admin/orders-managment",
+        element: <OrdersManagment />,
         children: [{ path: "cart", element: <CartModal /> }]
       }
     ],
--- src/utils/OrderService.ts
+++ src/utils/OrderService.ts
@@ -5,6 +5,7 @@ import { HOST } from "./host";
 const HOST_PORT = HOST;
 const ADD_NEW_ORDER = "http://" + HOST_PORT + "/api/order/add"
 const GET_ALL_ORDERS_FOR_USER = "http://" + HOST_PORT + "/api/order/list"
+const CHANGE_COMPLETE_STATUS = "http://" + HOST_PORT + "/api/order/"
 
 
 export const addNewOrder = async (order: Order) => {
@@ -27,6 +28,28 @@ export const getAllOrdersForUser = async (email: string) => {
     try {
         const response = await axios.get(GET_ALL_ORDERS_FOR_USER + `/${email}`)
         console.log("GET ALL ORDERS FO USER FUNC");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const getAllOrders = async () => {
+    try {
+        const response = await axios.get(GET_ALL_ORDERS_FOR_USER);
+        console.log("GET ALL ORDERS  FUNC");
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export const changeCompleteStatus = async (orderId: number) => {
+    try {
+        const response = await axios.patch(`${CHANGE_COMPLETE_STATUS}${orderId}`);
+        console.log("CHANGE COMPLETE STATUS  FUNC");
         return response.data;
     } catch (error) {
         console.error(error);
