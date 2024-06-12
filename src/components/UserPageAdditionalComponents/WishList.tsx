fca85547db85d826de427288864ac4e4effa03a7
Miziev <mizzzev@outlook.com>
2024-06-12 15:40:18 +0300

    - added text block in wishlist (it exists if wishList empty)

--- src/components/UserPageAdditionalComponents/WishList.tsx
+++ src/components/UserPageAdditionalComponents/WishList.tsx
@@ -3,11 +3,13 @@ import { Phone } from "../../shared.types";
 import { getWishListForUser } from "../../utils/UserService";
 import WishListItem from "./WishListItem";
 import classes from "./WishList.module.scss"
+import { useNavigate } from "react-router";
 
 const WishList: React.FC = () => {
 
     const [wishList, setWishList] = useState<Phone[]>([])
     const email = sessionStorage.getItem("authenticatedEmail");
+    const navigator = useNavigate();
 
     useEffect(() => {
 
@@ -20,12 +22,25 @@ const WishList: React.FC = () => {
         setWishList((prevWishList) => prevWishList.filter((phone) => phone.id !== phoneId));
     };
 
+    const onClickButtonHandler = () => {
+        navigator("/phone/catalog")
+    }
+
     return (
         <div className={classes.container}>
             {
-                wishList.map((phone) => (
-                    <WishListItem key={phone.id} phone={phone} onDelete={deletePhoneHandler} />
-                ))
+                wishList && wishList.length > 0 ? (
+
+                    wishList.map((phone) => (
+                        <WishListItem key={phone.id} phone={phone} onDelete={deletePhoneHandler} />
+                    ))
+                ) : (
+                    <div>
+                        <h2>Список бажань</h2>
+                        <p>Список бажань порожній. Можете обрати до цього списку смартфон із каталогу.</p>
+                        <button onClick={onClickButtonHandler}>Каталог смартфонів</button>
+                    </div>
+                )
             }
         </div>
     )
